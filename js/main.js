50c7ce17b10b76faea939462a18d64a94c676bad
tomo <cxytomo@gmail.com>
2014-05-26 21:16:32 +0800

    redesign the page
    The new recruitment page is much more clean. carousel and whole page
    scroll are denied. Simple is best!

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-//carouse begin
-var mySwiper = new Swiper('.swiper-container',{
-  mode:'horizontal',
-  loop: true,
-  autoplay : 5000,
-  speed: 1000
-})
-
-$('.js-prev').on('click', function(e){
-  e.preventDefault();
-  mySwiper.swipePrev();
-})
-
-$('.js-next').on('click', function(e){
-  e.preventDefault();
-  mySwiper.swipeNext();
-})
-//carouse end
-
 //job detail list
 $('.js-job-detail').on('click', function(e) {
             var $tar = $(e.target);
